b4b9dbc8daccca6d765b9b1dbeb95209037aa0e8
durban24k <durban24k@users.noreply.github.com>
2020-07-12 19:43:29 +0300

    v3.0 post request

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 let express = require('express');
 let request = require('request');
+let bodyParser = require('body-parser');
 
 let app = express();
 
 app.set('view engine','ejs');
 app.use(express.static("public"));
-
-let city = 'Nairobi';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=eb50d0ab0c62bab6425cfe7c8d233c62`;
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
+    let city = 'New York';
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=eb50d0ab0c62bab6425cfe7c8d233c62`;
     request(url, (error, response, body)=>{
-        weatherinfo = JSON.parse(body);
+        let weatherinfo = JSON.parse(body);
         let weather = {
             city:city,
             temperature: Math.round(weatherinfo.main.temp),
@@ -26,7 +27,25 @@ app.get("/",(req,res)=>{
     });
 });
 
-
+app.post("/city",(req,res)=>{
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=eb50d0ab0c62bab6425cfe7c8d233c62`;
+    request(url, (error, response, body)=>{
+        let weatherinfo = JSON.parse(body);
+        let weather = {
+            city:city,
+            temperature: Math.round(weatherinfo.main.temp),
+            description: weatherinfo.weather[0].description,
+            icon: weatherinfo.weather[0].icon,
+            country:weatherinfo.sys.country,
+            tempmin:weatherinfo.main.temp_min,
+            tempmax:weatherinfo.main.temp_max
+        };
+        console.log(`${city} has been checked`);
+        let weatherdata = {weather:weather};
+        res.render("weather.ejs",weatherdata);
+    });
+});
 
 app.listen(3000, ()=>{
     console.log("Server Listening on Port 3000....");
