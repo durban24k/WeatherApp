3426071f62ef014ef6ce759dd3c2a5d2b64c0035
durban24k <durban24k@users.noreply.github.com>
2020-07-12 19:52:58 +0300

    v3.1 README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WeatherApp
  Basic Weather Web Application
+It gives weather conditions feedback for the different cities you search for.
+It uses the openweathermap API to give information.
--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
-    let city = 'New York';
+    let city = 'Nairobi';
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=eb50d0ab0c62bab6425cfe7c8d233c62`;
     request(url, (error, response, body)=>{
         let weatherinfo = JSON.parse(body);
