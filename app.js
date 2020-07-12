11f4fca4a2ba2dfcf54c1343e4d1927279cab593
durban24k <durban24k@users.noreply.github.com>
2020-07-12 13:33:46 +0300

    v1.1

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ let express = require('express');
 
 let app = express();
 
+app.set('view engine','ejs');
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("weather.ejs");
+});
+
 app.listen(3000, ()=>{
     console.log("Server Listening on Port 3000....");
 });
